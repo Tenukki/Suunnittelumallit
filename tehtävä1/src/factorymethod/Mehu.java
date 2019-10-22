1982d840f5c7c1900657bca27cd640ebeb09b76a
Tenukki <tenhunens@hotmail.com>
2019-10-22 10:57:26 +0300

    valmis

---
+++ tehtävä1/src/factorymethod/Mehu.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod;
+
+/**
+ *
+ * @author Santeri
+ */
+public class Mehu implements Juoma{
+
+    @Override
+    public String toString() {
+        return "mehu";
+    }
+
+    
+}
---
+++ tehtävä1/src/factorymethod/Sima.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod;
+
+/**
+ *
+ * @author Santeri
+ */
+public class Sima implements Juoma{
+
+    @Override
+    public String toString() {
+        return "sima";
+    }
+    
+}
